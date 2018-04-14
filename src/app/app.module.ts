cabc3bb705e21e5011375eba7cf23ae940c46016
keanuk <keanukerr@gmail.com>
2018-04-14 03:56:48 -0400

    Races now display from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 
@@ -34,6 +35,7 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [UserService, DndService, AuthGuard],
   bootstrap: [AppComponent]
--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -4,22 +4,33 @@ import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { DndService } from '../dnd.service';
 
+
 @Component({
   selector: 'app-race',
   templateUrl: './race.component.html',
   styleUrls: ['./race.component.css']
 })
+
+
 export class RaceComponent implements OnInit {
 
+	loaded = false;
+  races = [];
   constructor(private router: Router, private user: UserService, private dnd: DndService) { }
 
   ngOnInit() {
+    this.dnd.getRaceList((data) => {
+      console.log(data);
+      this.races = data;
+      this.loaded = true;
+    });
   }
 
   setRace() {
     // let raceName
     this.dnd.getRaceList((data) => {
       console.log(data);
+      return data;
     });
   }
 
